2b2ff1e83d41b7427266a140511771df5c691054
prince rana <hyperqash1@gmail.com>
2026-06-22 10:01:38 +0300

    add a comit

--- actions/login.php
+++ actions/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require '../config/db.php';
+require_once __DIR__ . '/../config/db.php';
 
 function wants_json_response()
 {
--- actions/register_user.php
+++ actions/register_user.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require '../config/db.php';
+require_once __DIR__ . '/../config/db.php';
 
 
 // 0. Redirect if already logged in
